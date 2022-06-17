6011aa195a841ca4b1af3944ce5c3cc3d95d216c
Thewads <rosswaddell1990@gmail.com>
2022-06-17 20:28:14 +0100

    Basic UI PoC

---
+++ ColorHelper/ColorHelperUi/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ColorHelperUi;
+using ColorService;
+using ColorService.Converters;
+using ColorService.Providers;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IColorService, ColorService.ColorService>();
+builder.Services.AddScoped<IPaintProvider, JsonProvider>();
+builder.Services.AddScoped<IRgbToLabConverter, RgbToLabConverter>();
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
